efe16508f759990333d0ca838b19453fb3d0941b
James Yu <jamesyu608@gmail.com>
2020-06-13 16:29:35 +0800

    [Feature] Finish HeroDAL with hahow APIs
    TODO:
    - heroes routes

--- src/components/heroes/HeroDAL.js
+++ src/components/heroes/HeroDAL.js
@@ -1,32 +1,64 @@
 // Hero's DAL (Data Access Layer), this is where the hero's data come from (e.g. database or API) and CRUD.
 // Separate from web and logic layer so that we can change data source without affecting the existing logic
 
-const Hero = require('Hero')
+const Hero = require('./Hero')
+const hahowAPI = require('../../services/hahowAPI')
 
 class HeroDAL {
-  async getAllHeroes (isSetProfile = false) {
-    const heroes = [new Hero()]
-    if (isSetProfile) {
+  async getAllHeroes (hasProfile = false) {
+    const rawHeroesData = await hahowAPI.getAllHeroes()
+    if (rawHeroesData.length === 0) {
+      return []
+    }
+
+    const heroes = rawHeroesData.map(data => {
+      return new Hero({
+        heroId: data.id,
+        name: data.name,
+        image: data.image
+      })
+    })
+    if (hasProfile) {
       await this.setHeroesProfile(heroes)
     }
+
     return heroes
+  }
+
+  async getHero (heroId, hasProfile = false) {
+    const rawHeroData = await hahowAPI.getHeroById(heroId)
+    if (!rawHeroData) {
+      return null
+    }
+
+    const hero = new Hero({
+      heroId: rawHeroData.id,
+      name: rawHeroData.name,
+      image: rawHeroData.image
+    })
+    if (hasProfile) {
+      await this.setHeroProfile(hero)
+    }
+
+    return hero
   }
 
   async setHeroesProfile (heroes) {
     for (const hero of heroes) {
-      hero.setProfile()
-    }
-  }
-
-  async getHero (heroId, isSetProfile = false) {
-    const hero = new Hero()
-    if (isSetProfile) {
       await this.setHeroProfile(hero)
     }
   }
 
   async setHeroProfile (hero) {
-    hero.setProfile()
+    const rawHeroProfileData = await hahowAPI.getHeroProfileById(hero.heroId)
+    if (rawHeroProfileData) {
+      hero.setProfile({
+        str: rawHeroProfileData.str,
+        int: rawHeroProfileData.int,
+        agi: rawHeroProfileData.agi,
+        luk: rawHeroProfileData.luk
+      })
+    }
   }
 }
 
